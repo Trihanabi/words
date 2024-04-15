4393436f7e2054345b2ed49c8676212cb90867fb
Trihanabi <57278269+Trihanabi@users.noreply.github.com>
2024-04-15 16:04:25 +0800

    remove word image

--- word-image/09-WordGraph-vue-get-data/src/main/java/com/joe/wordGraph/dao/WordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.joe.wordGraph.dao;
-
-import com.joe.wordGraph.entity.Word;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WordRepository extends JpaRepository<Word, Integer> {
-
-}

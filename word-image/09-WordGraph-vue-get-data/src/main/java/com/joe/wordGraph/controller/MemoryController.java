05d0caf23dd6e429e69d50ff471570418f27adbf
Trihanabi <57278269+Trihanabi@users.noreply.github.com>
2024-04-15 16:00:48 +0800

    updated to 09-WordGraph-vue-get-data

---
+++ word-image/09-WordGraph-vue-get-data/src/main/java/com/joe/wordGraph/controller/MemoryController.java
@@ -0,0 +1,52 @@
+package com.joe.wordGraph.controller;
+
+
+import com.joe.wordGraph.entity.Book;
+import com.joe.wordGraph.entity.Graph;
+import com.joe.wordGraph.entity.Users_Books;
+import com.joe.wordGraph.service.BookService;
+import com.joe.wordGraph.service.UserBookService;
+import com.joe.wordGraph.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/memory")
+public class MemoryController {
+    @Autowired
+    private BookService bookService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserBookService userBookService;
+
+    public MemoryController(BookService theBookService,
+                          UserService theUserService,
+                          UserBookService theUserBookService) {
+
+        bookService = theBookService;
+
+        userService = theUserService;
+
+        userBookService = theUserBookService;
+    }
+
+    @GetMapping("/{userId}&{bookId}")
+    public String startMemory(Model theModel,
+                              @PathVariable int userId,
+                              @PathVariable int bookId ) {
+        List<Graph> graphs = userBookService.getGraphs(userId, bookId);
+
+        theModel.addAttribute("graphs", graphs);
+
+        return "memory/word_memory";
+    }
+
+}
